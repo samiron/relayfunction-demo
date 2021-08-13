5a455193cbc27f31388c1449460488b952afea4d
Samiron Paul <samironpaul@gmail.com>
2021-08-13 11:03:27 -0300

    connecting methods added

--- src/main/java/of/samiron/demo/relayfunctiondemo/function/RelayOne.java
+++ src/main/java/of/samiron/demo/relayfunctiondemo/function/RelayOne.java
@@ -1,6 +1,24 @@
 package of.samiron.demo.relayfunctiondemo.function;
 
+import java.util.Objects;
+
 @FunctionalInterface
 public interface RelayOne<T> {
 	void apply(T t);
+
+	default RelayOne<T> next(RelayOne<T> nextFn) {
+		Objects.requireNonNull(nextFn);
+		return (t) -> {
+			this.apply(t);
+			nextFn.apply(t);
+		};
+	}
+
+	default <U> RelayTwo<T, U> next(RelayTwo<T, U> nextFn) {
+		Objects.requireNonNull(nextFn);
+		return (t, u) -> {
+			this.apply(t);
+			nextFn.apply(t, u);
+		};
+	}
 }
--- src/main/java/of/samiron/demo/relayfunctiondemo/function/RelayTwo.java
+++ src/main/java/of/samiron/demo/relayfunctiondemo/function/RelayTwo.java
@@ -1,6 +1,25 @@
 package of.samiron.demo.relayfunctiondemo.function;
 
+import java.util.Objects;
+
 @FunctionalInterface
 public interface RelayTwo<T, U> {
 	void apply(T t, U u);
+
+	default RelayTwo<T, U> next(RelayTwo<T, U> nextFn) {
+		Objects.requireNonNull(nextFn);
+		return (t, u) -> {
+			this.apply(t, u);
+			nextFn.apply(t, u);
+		};
+	}
+
+	default RelayTwo<T, U> next(RelayOne<T> nextFn) {
+		Objects.requireNonNull(nextFn);
+		return (t, u) -> {
+			this.apply(t, u);
+			nextFn.apply(t);
+		};
+	}
+
 }
